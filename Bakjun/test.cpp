f194b2cbfea43d5a99e3bfab95db9f88e632d4e7
EHLeeINTELLIZ <ehlee@intelliz.co.kr>
2022-01-04 14:47:21 +0900

    Create test.cpp

---
+++ Bakjun/test.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include<vector>
+#include<ctime>
+
+using namespace std;
+
+void test(char* test) {
+	
+	cout << *(test+1) << endl;
+
+}
+
+void main() {
+	
+
+	test(((char*)"hello"));
+
+	
+
+	
+
+}
